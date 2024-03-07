1969a1f9819dfe8cd562777cbe6b96150f9ed855
AngeloYagual <yagualangelo@gmail.com>
2024-03-07 09:46:15 -0500

    Training code

---
+++ modeltraining.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.callbacks import EarlyStopping
+
+
+
